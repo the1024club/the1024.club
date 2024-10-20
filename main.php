86354e513e5a1414520a8df63b1826367076ace8
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2024-10-20 10:15:42 -0600

    Update main.php
    added source code links.

--- main.php
+++ main.php
@@ -16,7 +16,8 @@
 
         <nav>
             <ul>
-                <li><a href="/">Home</a></li>
+                <li><a href="https://github.com/the1024club/the1024.club" target="_blank">Source Code</a></li>
+
             </ul>
         </nav>
 
@@ -168,7 +169,7 @@ php 1kb_client.php retrieve public_key.pem
 
         <footer>
             <p class="license-title">The MIT License (MIT)</p>
-            <p class="license-copyright">©2020 The 1024 Club Developers (see AUTHORS.txt)</p>
+            <p class="license-copyright">©2024 The 1024 Club Developers <a href="https://github.com/the1024club/the1024.club" target="_blank">Source Code</a></p>
             <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
             <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
             <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
